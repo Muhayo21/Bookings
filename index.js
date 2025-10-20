fcfa5876a42e3a54e7be2676032fd09e8414db08
Muhayo21 <134100914+Muhayo21@users.noreply.github.com>
2025-10-20 22:34:07 +0200

    Update index.js max players to 14

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const status = document.getElementById('status');
 const message = document.getElementById("message");
 const countDisplay = document.getElementById("count");
 
-const MaxReservations = 7;
+const MaxReservations = 14;
 let CurrentReservations = 0;
 
 // 🟢 Load current reservation count from JSONBin
@@ -126,4 +126,5 @@ form.addEventListener('submit', async (e) => {
     }
   }
 });
+
 
